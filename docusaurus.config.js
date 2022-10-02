833e09f540df8f039bb87263a66f73833144619a
Pearu Pung <pungpearu@gmail.com>
2022-10-02 10:11:19 +0300

    Initial commit, view deadlines and custom icons #1 #2

---
+++ docusaurus.config.js
@@ -0,0 +1,120 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Bakalaureuse lõputöö',
+  tagline: 'Parim lõputöö maailmas',
+  url: 'https://localhost:3001',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'pearupung', // Usually your GitHub org/user name.
+  projectName: 'bachelor-thesis', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'et',
+    locales: ['et'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+            path: 'docs/thesis',
+            routeBasePath: 'thesis',
+            // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+    plugins: [[
+        '@docusaurus/plugin-content-docs',
+        {
+            id: 'notes',
+            path: 'docs/notes',
+            routeBasePath: 'notes',
+        },
+        ],
+    ],
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Bakalaureuse lõputöö',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/university.jpg',
+        },
+        items: [
+          {
+            type: 'doc',
+            to: 'docs/thesis/',
+            docId: 'intro',
+            position: 'left',
+            label: 'Lõputöö',
+            sidebarPath: require.resolve('./sidebars.js')
+          },
+          {
+            type: 'doc',
+            docsPluginId: 'notes',
+            to: 'docs/notes/',
+            docId: 'intro',
+            position: 'left',
+            sidebarPath: require.resolve('./notesSidebar.js'),
+            label: 'Märkmed'},
+            {
+            href: 'https://github.com/pearupung/bachelor-thesis',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Thesis',
+                to: '/docs/intro',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Pearu Pung. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
---
+++ src/components/Countdown/index.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
+import { Temporal } from '@js-temporal/polyfill'
+const FeatureList = [
+  {
+    title: "Kavandi esitamine",
+    deadline: "2022-10-24T00:00:00.000",
+    Svg: require('@site/static/img/plan-icon.svg').default,
+    description: (
+      <>
+        Kavand annab ülevaate plaanitavast tööst - lõputöö teema, kirjandus ja
+        sisukord on detailid, mis peavad selleks ajaks paigas olema.
+      </>
+    ),
+  },
+  {
+    title: 'Lõputöö esitamine eelkaitsmiseks',
+    deadline: "2022-12-01T00:00:00.000",
+    Svg: require('@site/static/img/document.svg').default,
+    description: (
+      <>
+        Lõputöö esitatakse Moodle keskkonnas elektroonselt.
+      </>
+    ),
+  },
+  {
+    title: 'Lõputöö eelkaitsmised',
+    deadline: "2022-12-06T00:00:00.000",
+    Svg: require('@site/static/img/presentation-talk.svg').default,
+    description: (
+      <>
+        Lõputöö eelkaitsmine laseb harjutada kaitsmist ja tuvastab vigu.
+      </>
+    ),
+  },
+  {
+    title: 'Lõputöö esitamine kaitsmiseks',
+    deadline: "2023-01-05T00:00:00.000",
+    Svg: require('@site/static/img/reviewed-document.svg').default,
+    description: (
+      <>
+        Moodle keskkonnas tuleb esitada lõputöö PDF-vormingus,
+        digiallkirjaga koos digiallkirjastatud juhendaja arvamusega ning
+        täita metaandmete tabel.
+        </>
+    ),
+  },
+  {
+    title: 'Lõputöö kaitsmised',
+    deadline: "2023-01-17T00:00:00.000",
+    Svg: require('@site/static/img/presentation.svg').default,
+    description: (
+      <>
+        Lõputöö kaitsmine - viimane proovikivi!
+      </>
+    ),
+  },
+];
+
+function Feature({Svg, title, description, deadline}) {
+    const now_date = Temporal.Now.plainDateISO()
+    return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <h4>{Temporal.PlainDate.from(now_date).until(deadline).total({unit: 'day'})} päeva tähtajani</h4>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
